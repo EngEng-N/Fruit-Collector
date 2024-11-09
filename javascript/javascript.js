ff2198c607d06c27db6969227a8770baa861a5fa
EngEng-N <eeng3906@gmail.com>
2024-11-08 16:11:18 -0800

    add difficulty button changes when clicking with javascript

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -13,7 +13,7 @@ const Easybutton = document.getElementById("btn-easy");
 const Mediumbutton = document.getElementById("btn-medium");
 const Hardbutton = document.getElementById("btn-hard");
 
-console.log(getComputedStyle(BgDisplay).display)
+// console.log(getComputedStyle(BgDisplay).display)
 
 HTPbutton.addEventListener("click", function(){
 
@@ -29,4 +29,62 @@ HTPbutton.addEventListener("click", function(){
 
 })
 
-console.log(BgDisplay);
+// Difficulty changes
+
+Easybutton.addEventListener("click", difficultySelect);
+Mediumbutton.addEventListener("click", difficultySelect);
+Hardbutton.addEventListener("click", difficultySelect);
+
+function difficultySelect(difficulty){
+    if(difficulty === 'easy'){
+        Easybutton.style.background = "rgb(45, 150, 248)";
+        Mediumbutton.style.background = "rgb(255,255,255)";
+        Hardbutton.style.background = "rgb(255,255,255";
+    }
+    else if(difficulty === 'medium'){
+        Mediumbutton.style.background = "rgb(45, 150, 248)";
+        Easybutton.style.background = "rgb(255,255,255)";
+        Hardbutton.style.background = "rgb(255,255,255";
+    }
+    else if(difficulty === 'hard'){
+        Hardbutton.style.background = "rgb(45, 150, 248)";
+        Easybutton.style.background = "rgb(255,255,255)";
+        Mediumbutton.style.background = "rgb(255,255,255";
+    }
+}
+
+const difficultyBtns = document.getElementById('difficulty-btns');
+
+difficultyBtns.addEventListener('click', function(e){
+
+    //console.log(e.target)
+
+    if(e.target.tagName === 'BUTTON'){
+        //console.log(e.target.dataset.difficulty);
+        difficultySelect(e.target.dataset.difficulty);
+    }
+
+});
+
+
+//console.log(Easybutton.onclick);
+
+// Easybutton.addEventListener("click", function(){
+//     Easybutton.style.background = "rgb(45, 150, 248)";
+//     Mediumbutton.style.background = "rgb(255,255,255)";
+//     Hardbutton.style.background = "rgb(255,255,255";
+//     Easybutton.selected;
+// })
+
+// Mediumbutton.addEventListener("click", function(){
+//     Mediumbutton.style.background = "rgb(45, 150, 248)";
+//     Easybutton.style.background = "rgb(255,255,255)";
+//     Hardbutton.style.background = "rgb(255,255,255";
+// })
+
+// Hardbutton.addEventListener("click", function(){
+//     Hardbutton.style.background = "rgb(45, 150, 248)";
+//     Easybutton.style.background = "rgb(255,255,255)";
+//     Mediumbutton.style.background = "rgb(255,255,255";
+// })
+
