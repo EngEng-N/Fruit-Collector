cbcc498e32e57ef8805181d4ef4cfb6387b052bf
EngEng-N <eeng3906@gmail.com>
2024-11-11 19:46:59 -0800

    finish setting up the game display, added obstacle moving to the left,  need to add velocity to make Mario move

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,4 +1,5 @@
 // Game board
+let board = document.querySelector(".board");
 let boardWidth = 1000; // board width
 let boardHeight = 700; // board height
 
@@ -6,23 +7,87 @@ let boardHeight = 700; // board height
 let marioWidth = 50;
 let marioHeight = 50;
 let marioX = boardWidth/2; // center mario
-let marioY = 340; // mario above the dirt of the background
+let marioY = 490; // mario above the dirt of the background
 
 let Mario = {
     x : marioX,
     y : marioY,
-    width: marioWidth,
-    height: marioHeight
+    width : marioWidth,
+    height : marioHeight
 }
+
+// Obstacles
+let barrelArray = [];
+let barrelWidth = 60;
+let barrelHeight = 41;
+let barrelX = boardWidth; // draw the barrel at the right side of the screen
+let barrelY = 490; // draw the barrel at the same Y level as Mario
+
+// Game physcis
+let velocityX = -2; // Obstacles moving speed
 
 // Create the canvaso on load
 window.onload = function(){
-    board = document.getElementById("container");
     board.height = boardHeight;
     board.width = boardWidth;
     context = board.getContext("2d");
 
     // Draw Mario character
-    context.fillStyle = "green";
-    context.fillRect(Mario.x, Mario.y, Mario.width, Mario.height);
+    marioIMG = new Image();
+    marioIMG.src = "../images/MarioIdle (1).gif"
+    marioIMG.onload = function(){
+        context.drawImage(marioIMG, Mario.x, Mario.y, Mario.width, Mario.height);
+    }
+
+    // Barrel
+    barrelIMG = new Image();
+    barrelIMG.src = "../images/barrel.png";
+    
+    requestAnimationFrame(update);
+    // setInterval(placeBarrel, 1500); // Barrel will be placed every 1.5s
+    document.addEventListener("keydown", moveMario);
+}
+
+// Re draw the canvas function
+function update(){
+    requestAnimationFrame(update);
+    // start clearing the france from 0,0
+    // width is board.width or 1000px 
+    // height is board.height or 700px
+    context.clearRect(0,0, board.width, board.height); 
+
+    // Re draw Mario image
+    
+    context.drawImage(marioIMG, Mario.x, Mario.y, Mario.width, Mario.height);
+
+    for(let i = 0; i < barrelArray.length; i++){
+        let Barrel = barrelArray[i];
+        Barrel.x += velocityX; // Shifting position X of the barrel before drawing
+        context.drawImage(Barrel.img, Barrel.x, Barrel.y, Barrel.width, Barrel.height);
+    }
+}
+
+function placeBarrel(){
+    let barrelOb = {
+        img : barrelIMG,
+        x : barrelX,
+        y : barrelY,
+        width : barrelWidth,
+        height : barrelHeight,
+        destroyed : false
+    }
+    barrelArray.push(barrelOb);
+}
+
+//  Mario character keys for movement
+function moveMario(e){
+    if(e.code == "KeyA"){
+        velocityX = -5;
+    }
+    else if(e.code == "KeyD"){
+        velocityX = 5;
+    }
+    // else if(e.code == "Space"){
+
+    // }
 }
